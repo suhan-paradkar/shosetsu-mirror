f55fd9597fbad1bf48c72cab69851a6d4f8b70e5
Jobobby04 <jobobby04@users.noreply.github.com>
2022-09-15 14:16:15 -0400

    Convert webview to compose

--- android/src/main/java/app/shosetsu/android/ui/webView/WebViewApp.kt
+++ android/src/main/java/app/shosetsu/android/ui/webView/WebViewApp.kt
@@ -1,28 +1,39 @@
 package app.shosetsu.android.ui.webView
 
 import android.annotation.SuppressLint
-import android.content.Context
+import android.content.Intent
+import android.content.pm.ApplicationInfo
 import android.os.Bundle
-import android.util.AttributeSet
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
 import android.webkit.CookieManager
 import android.webkit.WebView
-import android.webkit.WebViewClient
-import androidx.activity.addCallback
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import app.shosetsu.android.BuildConfig
 import app.shosetsu.android.R
-import app.shosetsu.android.activity.MainActivity
 import app.shosetsu.android.common.consts.BundleKeys.BUNDLE_URL
 import app.shosetsu.android.common.consts.USER_AGENT
-import app.shosetsu.android.common.ext.launchUI
 import app.shosetsu.android.common.ext.logI
 import app.shosetsu.android.common.ext.logV
 import app.shosetsu.android.common.ext.openInBrowser
+import app.shosetsu.android.common.ext.toast
 import app.shosetsu.android.common.utils.CookieJarSync
-import app.shosetsu.android.databinding.ActivityWebviewBinding
-import app.shosetsu.android.databinding.ActivityWebviewBinding.inflate
+import com.google.accompanist.web.*
+import com.google.android.material.composethemeadapter.MdcTheme
 import okhttp3.Cookie
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import org.kodein.di.DI
@@ -58,70 +69,166 @@ import org.kodein.di.android.closestDI
 class WebViewApp : AppCompatActivity(), DIAware {
 	override val di: DI by closestDI()
 
-	private lateinit var binding: ActivityWebviewBinding
-
-	override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
-		return super.onCreateView(name, context, attrs)?.also {
-			binding = ActivityWebviewBinding.bind(it)
-		}
-	}
-
-	override fun onOptionsItemSelected(item: MenuItem): Boolean {
-		when (item.itemId) {
-			android.R.id.home -> finish()
-			R.id.open_browser -> app.shosetsu.android.common.ext.launchIO {
-				openInBrowser(intent.getStringExtra(BUNDLE_URL)!!)
-				finish()
+	private fun shareWebpage(url: String) {
+		try {
+			val intent = Intent(Intent.ACTION_SEND).apply {
+				type = "text/plain"
+				putExtra(Intent.EXTRA_TEXT, url)
 			}
+			startActivity(Intent.createChooser(intent, getString(R.string.share)))
+		} catch (e: Exception) {
+			e.message?.let { toast(it) }
 		}
-		return super.onOptionsItemSelected(item)
 	}
 
-	override fun onCreateOptionsMenu(menu: Menu): Boolean {
-		menuInflater.inflate(R.menu.webview_menu, menu)
-		return true
-	}
-
-	@SuppressLint("SetJavaScriptEnabled", "JavascriptInterface")
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(inflate(layoutInflater).also { binding = it }.root)
-		this.setSupportActionBar(binding.toolbar)
-		supportActionBar?.apply {
-			setDisplayHomeAsUpEnabled(true)
+
+		val url = intent.getStringExtra(BUNDLE_URL) ?: kotlin.run {
+			toast(R.string.activity_webview_null_url)
+			finish()
+			return
 		}
 
-		onBackPressedDispatcher.addCallback {
-			if (binding.webview.canGoBack()) {
-				binding.webview.goBack()
-			} else finish()
+		setContent {
+			MdcTheme {
+				WebViewScreen(
+					onUp = ::finish,
+					url = url,
+					onShare = ::shareWebpage,
+					onOpenInBrowser = ::openInBrowser
+				)
+			}
 		}
+	}
+}
 
-		binding.webview.settings.userAgentString = USER_AGENT
-		binding.webview.settings.javaScriptEnabled = true
-		CookieManager.getInstance().setAcceptThirdPartyCookies(binding.webview, true)
-
-		binding.webview.webViewClient = object : WebViewClient() {
-			override fun onPageFinished(view: WebView, url: String) {
-				logI(url)
-				view.evaluateJavascript("document.cookie") { cookies ->
-					val httpUrl = url.toHttpUrl()
-					CookieJarSync.saveFromResponse(
-						httpUrl,
-						cookies.split("; ").mapNotNull { Cookie.parse(httpUrl, it) }
+@SuppressLint("SetJavaScriptEnabled")
+@Composable
+fun WebViewScreen(
+	onUp: () -> Unit,
+	url: String,
+	onShare: (String) -> Unit,
+	onOpenInBrowser: (String) -> Unit,
+) {
+	val state = rememberWebViewState(url = url)
+	val navigator = rememberWebViewNavigator()
+	Scaffold(
+		topBar = {
+			Box {
+				TopAppBar(
+					title = {
+						Text(
+							text = state.pageTitle ?: stringResource(R.string.app_name),
+							maxLines = 1,
+							overflow = TextOverflow.Ellipsis
+						)
+					},
+					navigationIcon = {
+						IconButton(onClick = onUp) {
+							Icon(imageVector = Icons.Default.Close, contentDescription = null)
+						}
+					},
+					actions = {
+						IconButton(
+							onClick = {
+								if (navigator.canGoBack) {
+									navigator.navigateBack()
+								}
+							},
+							enabled = navigator.canGoBack,
+						) {
+							Icon(imageVector = Icons.Default.ArrowBack, contentDescription = stringResource(R.string.action_webview_back))
+						}
+						IconButton(
+							onClick = {
+								if (navigator.canGoForward) {
+									navigator.navigateForward()
+								}
+							},
+							enabled = navigator.canGoForward,
+						) {
+							Icon(imageVector = Icons.Default.ArrowForward, contentDescription = stringResource(R.string.action_webview_forward))
+						}
+						var overflow by remember { mutableStateOf(false) }
+						IconButton(onClick = { overflow = !overflow }) {
+							Icon(Icons.Default.MoreVert, contentDescription = stringResource(R.string.more))
+						}
+						DropdownMenu(expanded = overflow, onDismissRequest = { overflow = false }) {
+							DropdownMenuItem(onClick = { navigator.reload(); overflow = false }) {
+								Text(text = stringResource(R.string.action_webview_refresh))
+							}
+							DropdownMenuItem(onClick = { onShare(state.content.getCurrentUrl()!!); overflow = false }) {
+								Text(text = stringResource(R.string.share))
+							}
+							DropdownMenuItem(onClick = { onOpenInBrowser(state.content.getCurrentUrl()!!); overflow = false }) {
+								Text(text = stringResource(R.string.open_in_browser))
+							}
+						}
+					}
+				)
+				when (val loadingState = state.loadingState) {
+					is LoadingState.Initializing -> LinearProgressIndicator(
+						modifier = Modifier
+							.fillMaxWidth()
+							.align(Alignment.BottomCenter),
 					)
-					logV("Cookies: $cookies")
+					is LoadingState.Loading -> {
+						val animatedProgress by animateFloatAsState(
+							(loadingState as? LoadingState.Loading)?.progress ?: 1f,
+							animationSpec = ProgressIndicatorDefaults.ProgressAnimationSpec,
+						)
+						LinearProgressIndicator(
+							progress = animatedProgress,
+							modifier = Modifier
+								.fillMaxWidth()
+								.align(Alignment.BottomCenter),
+						)
+					}
+					else -> {}
+				}
+			}
+
+		}
+	) { contentPadding ->
+		val webClient = remember {
+			object : AccompanistWebViewClient() {
+				override fun onPageFinished(view: WebView?, url: String?) {
+					super.onPageFinished(view, url)
+					logI(url)
+					if (view == null || url == null) return
+					view.evaluateJavascript("document.cookie") { cookies ->
+						val httpUrl = url.toHttpUrl()
+						CookieJarSync.saveFromResponse(
+							httpUrl,
+							cookies.split("; ").mapNotNull { Cookie.parse(httpUrl, it) }
+						)
+						logV("Cookies: $cookies")
+					}
 				}
 			}
 		}
 
-		intent.getStringExtra(BUNDLE_URL)?.let {
-			binding.webview.loadUrl(it)
-		} ?: run {
-			launchUI {
-				(parent as? MainActivity)?.makeSnackBar(R.string.activity_webview_null_url)?.show()
-			}
-			onBackPressedDispatcher.onBackPressed()
-		}
+		WebView(
+			state = state,
+			modifier = Modifier.padding(contentPadding),
+			navigator = navigator,
+			onCreated = { webView ->
+				webView.settings.apply {
+					userAgentString = USER_AGENT
+					javaScriptEnabled = true
+				}
+
+				CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true)
+
+				// Debug mode (chrome://inspect/#devices)
+				if (BuildConfig.DEBUG &&
+					0 != webView.context.applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE
+				) {
+					WebView.setWebContentsDebuggingEnabled(true)
+				}
+			},
+			client = webClient,
+		)
 	}
 }
