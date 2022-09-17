bf1b308a0429093d2044d985bce2657f70fbd875
doomsdayrs <doomsdayrs@gmail.com>
2022-09-17 13:45:18 -0400

    v2.1.2

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -41,8 +41,8 @@ android {
 		applicationId = "app.shosetsu.android"
 		minSdk = 22
 		targetSdk = 33
-		versionCode = 32
-		versionName = "2.1.1"
+		versionCode = 33
+		versionName = "2.1.2"
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 		multiDexEnabled = true
 
