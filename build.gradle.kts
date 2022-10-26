5b207d0481c3a642befaea3fd1fb32b4ea5429e0
doomsdayrs <doomsdayrs@gmail.com>
2022-10-26 13:26:41 -0400

    Upgrade AGP to 7.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ buildscript {
 	}
 
 	dependencies {
-		classpath("com.android.tools.build:gradle:7.3.0")
+		classpath("com.android.tools.build:gradle:7.3.1")
 		classpath(kotlin("gradle-plugin", version = kotlinVersion))
 		classpath(kotlin("serialization", version = kotlinVersion))
 	}
