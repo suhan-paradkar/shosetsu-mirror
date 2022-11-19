318d136651fe61324a258d4d355bd8bf57caab28
wisest_wizard <12suhangp34@gmail.com>
2022-11-19 18:35:11 +0530

    Fix missing import

--- android/src/main/java/app/shosetsu/android/ui/about/AboutController.kt
+++ android/src/main/java/app/shosetsu/android/ui/about/AboutController.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
