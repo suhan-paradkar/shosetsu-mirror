e285bedc18bad9477422c3259d689188209cd053
wisest_wizard <12suhangp34@gmail.com>
2022-11-23 17:19:46 +0530

    Migrate Extensionconfigure to Material3

--- android/src/main/java/app/shosetsu/android/ui/extensionsConfigure/ConfigureExtension.kt
+++ android/src/main/java/app/shosetsu/android/ui/extensionsConfigure/ConfigureExtension.kt
@@ -9,7 +9,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
